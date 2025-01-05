62a927bfb48e6efdb2ba313aa59bd03cb8008acd
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2025-01-05 11:03:49 +0530

    add middleware

--- Prometheus-Grafana/src/index.ts
+++ Prometheus-Grafana/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Counter } from "prom-client";
 import client from "prom-client";
 
@@ -8,15 +8,40 @@ app.use(express.json());
 let counter = new Counter({
   name: "numberOfRequests",
   help: "Number of requests made",
-  labelNames: ["method"],
+  labelNames: ["route", "method", "status_code"],
 });
 
+const requestCounterMiddleware = (req: Request, res: Response, next: NextFunction ) => {
+  res.on("finish", () => {
+    counter.inc({
+      route: req.path,
+      method: req.method,
+      status_code: res.statusCode,
+    });
+  });
+  next();
+};
+
+app.use(requestCounterMiddleware);
+
 app.get("/info", (req, res) => {
-  counter.inc();
-  res.send({
+  res.status(200).send({
     name: "Garvit",
     age: 20,
   });
+});
+
+app.get("/todos", (req, res) => {
+  res.status(200).send([
+    {
+      id: 1,
+      name: "Garvit",
+    },
+    {
+      id: 2,
+      name: "Dadheech",
+    },
+  ]);
 });
 
 app.get("/metrics", async (req, res) => {
